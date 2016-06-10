d95e7a08db812a22db2ce382d30c1a116e1a20a8
Snir Yarom <snir.yarom@agoda.com>
2016-06-10 13:45:59 +0700

    added switch case for snir

--- Agoda101/Security.cs
+++ Agoda101/Security.cs
@@ -11,7 +11,8 @@ namespace Agoda101
         {
             switch (name)
             {
-                case "Lee":
+                case "Leee":
+                case "Snir":
                     return true;
                 default:
                     return false;
